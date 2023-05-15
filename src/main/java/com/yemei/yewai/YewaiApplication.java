bbf7fa75b5afe3be48cda0217b12936846660c37
minghua.xie <minghua.xie@shopee.com>
2023-05-15 10:37:18 +0800

    first commit

---
+++ src/main/java/com/yemei/yewai/YewaiApplication.java
@@ -0,0 +1,13 @@
+package com.yemei.yewai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YewaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YewaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yemei/yewai/YewaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yemei.yewai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YewaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
