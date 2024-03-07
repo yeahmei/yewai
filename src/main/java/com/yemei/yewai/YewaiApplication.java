c90068c60255a54a64bc9bbadf23db8cfd284765
minghua.xie <minghua.xie@shopee.com>
2024-03-07 18:35:30 +0800

    2024

--- src/main/java/com/yemei/yewai/YewaiApplication.java
+++ src/main/java/com/yemei/yewai/YewaiApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * @author minghua.xie
+ */
 @SpringBootApplication
 @EnableScheduling
 public class YewaiApplication {
